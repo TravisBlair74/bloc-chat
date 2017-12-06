c15c24f38849bf62326f7a01429dcb0d0fcfa2a1
Travis-Blair <travisblair74@gmail.com>
2017-12-06 12:00:41 -0500

    Added create chat rooms functionality

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,8 +5,22 @@ class RoomList extends Component {
     super(props);
     this.state = {
       rooms: [],
+      newRoomName: ''
     };
     this.roomsRef = this.props.firebase.database().ref('rooms');
+  }
+
+  createRoom(e) {
+    e.preventDefault();
+    if (!this.state.newRoomName) { return }
+    this.roomsRef.push({
+      name: this.state.newRoomName
+    });
+    this.setState({ newRoomName: ''  });
+  }
+
+  handleChange(e) {
+    this.setState({ newRoomName: e.target.value })
   }
 
   componentDidMount() {
@@ -19,6 +33,11 @@ class RoomList extends Component {
 
   render() {
       return (
+        <div>
+        <form onSubmit={ (e) => this.createRoom(e) }>
+          <input type="text" value={ this.state.newRoomName } onChange={ (e) => this.handleChange(e) } />
+          <input type="submit" value="Create Room"/>
+        </form>
         <ul>
           {this.state.rooms.map((room, key) => (
             <li key={room.key}>
@@ -26,9 +45,9 @@ class RoomList extends Component {
             </li>
           ))}
         </ul>
+        </div>
       );
     }
-
 }
 
 export default RoomList;
